32cd6ed57566327bd48dc0aeac65543d6f060dc4
Anabah Zillah Teni <zillahanabah42@gmail.com>
2024-01-06 22:15:48 +0000

    add all files

---
+++ js/index.js
@@ -0,0 +1,12 @@
+const menu =document.getElementsByClassName('moblie-menu')
+const closeMenu=()=>{
+    menu.classlist.remove('active')
+}
+const openMenu=()=>{
+    menu.classlist.add('active')
+}
+
+function closeMenu(){}
+function openMenu(){
+    document.getElementsByClassName('moblie-menu').style.
+}
